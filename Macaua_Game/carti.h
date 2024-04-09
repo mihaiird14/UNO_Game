1591db2a6f3eb549238b5b480d23a5147496e9b4
mihaiird14 <148045836+mihaiird14@users.noreply.github.com>
2024-04-09 22:17:30 +0300

    Update carti.h

--- Macaua_Game/carti.h
+++ Macaua_Game/carti.h
@@ -1,8 +1,6 @@
 #include <string>
-#include <memory>
 #include <vector>
 #include <cstdlib>
-#include <ctime>
 using namespace std;
 class Carti{
     string culoare;
@@ -64,8 +62,49 @@ void GenerareCarti(vector<pair<int,int>>&v){
         swap(v[a1],v[a2]);
     }
 }
-void desen(int culoare,int semn,vector<string>&v){
+void desen(int culoare,int semn,vector<string>&f,vector<string>&n,vector<pair<int,int>>&fo,vector<pair<int,int>>&ne,pair<int,int>deVerificat){
     string c,s;
+    if(culoare==1)
+        c="INIMA RO";
+    else if(culoare==2)
+        c="INIMA NE";
+    else if(culoare==3)
+        c="..CARO..";
+    else
+        c=".TREFLA.";
+    if(semn==1)
+        s="AS";
+    else if(semn>=2 && semn<=9)
+        s=to_string(semn)+".";
+    else if(semn==10)
+        s="10";
+    else if(semn==11)
+        s="J.";
+    else if(semn==12)
+        s="Q.";
+    else
+        s="K.";
+    if(culoare==deVerificat.first || semn==deVerificat.second){
+        fo.push_back(make_pair(culoare,semn));
+        f.push_back("..........");
+        f.push_back("..........");
+        f.push_back("...."+s+"....");
+        f.push_back("."+c+".");
+        f.push_back("..........");
+        f.push_back("..........");
+    }
+    else{
+        ne.push_back(make_pair(culoare,semn));
+        n.push_back("..........");
+        n.push_back("..........");
+        n.push_back("...."+s+"....");
+        n.push_back("."+c+".");
+        n.push_back("..........");
+        n.push_back("..........");
+    }
+}
+void desen(int culoare,int semn,vector<string>&v){
+        string c,s;
     if(culoare==1)
         c="INIMA RO";
     else if(culoare==2)
@@ -92,5 +131,14 @@ void desen(int culoare,int semn,vector<string>&v){
     v.push_back("."+c+".");
     v.push_back("..........");
     v.push_back("..........");
+}
+pair<int,int> alegeCarteStart(vector<pair<int,int>> &ListaCarti){
+    srand(time(nullptr));
+    int poz=rand()%(ListaCarti.size());
+    while(ListaCarti[poz].second==1 || ListaCarti[poz].second==2 || ListaCarti[poz].second==3 || ListaCarti[poz].second==4)
+        poz=rand()%(ListaCarti.size());
+    pair<int,int>x=ListaCarti[poz];
+    ListaCarti.erase(ListaCarti.begin()+poz);
+    return x;
 }
 
